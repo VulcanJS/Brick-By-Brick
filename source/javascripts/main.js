5d0160211edbe86d6665e34e8856667386d2dba9
Sacha Greif <sacha357@gmail.com>
2015-02-19 03:57:13 +0100

    still working

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -1,3 +1,14 @@
+var updateTweetLink = function () {
+  var contents = $('.tweet-content').val();
+  var url = "http%3A%2F%2Fbook.discovermeteor.com%2Fstarter%3Futm_source%3Dstarter%26utm_medium%3Dtwitter%26utm_campaign%3Dstarter_tweet";
+  var twitterLink = "https://twitter.com/intent/tweet?original_referer=https%3A%2F%2Fbook.discovermeteor.com&url="+url+"&text="+encodeURIComponent(contents);
+  var charRemaining = 118 - contents.length;
+  $('.tweet-link').attr('href', twitterLink);
+  $('.tweet-counter').text(charRemaining);
+  charRemaining <= 0 ? $('.tweet-counter').addClass('danger') : $('.tweet-counter').removeClass('danger'); 
+}
+
+
 $(function(){
   $('body').addClass('animate');
   $(".video").fitVids();
@@ -14,4 +25,9 @@ $(function(){
   // Do the injection
   SVGInjector(mySVGsToInject);
 
+  updateTweetLink();
+  $('.tweet-content').keyup(function () {
+    updateTweetLink();
+  });
+  
 });
